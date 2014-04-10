07982f405c2c7f5f6c5af22164d9711c9b5cd0fb
ncatlin <nia.catlin.2012@live.rhul.ac.uk>
2014-04-10 22:23:43 +0100

    This layout makes more sense

--- phonetics.py
+++ phonetics.py
@@ -7,44 +7,44 @@ Created on 10 Apr 2014
 
 import sys
     
-if __name__ == '__main__':
-    
-    equivalents = {1:['A','E','I','O','U'],
+equivalents = {1:['A','E','I','O','U'],
                    2:['C','G','J','K','Q','S','X','Y','Z'],
                    3:['B','F','P','V','W'],
                    4:['D','T'],
                    5:['M','N']}
     
-    #reduce the name supplied to a string describing its equivalent sets
-    def reduceName(name):
+#reduce the name supplied to a string describing its equivalent sets
+def reduceName(name):
         
-        #rule 1: discard non alphabetic characters
-        name = ''.join(i for i in name if i.isalpha()) 
+    #rule 1: discard non alphabetic characters
+    name = ''.join(i for i in name if i.isalpha()) 
+    
+    if len(name) == 0: return ''
+    
+    name = name.upper() #rule 2: ignore case
+    
+    #rule 3: discard certain letters after the first
+    name = name[0] + name[1:].translate(None,'AEIHOUWY') 
+    
+    #rule 4: consider certain sets of letters equivalent
+    activeSet = 0
+    reducedName = ''
+    for character in name:
+        #rule 5: consecutive members of set are single occurrence
+        if activeSet != 0 and character in equivalents[activeSet]: continue 
         
-        if len(name) == 0: return ''
-        
-        name = name.upper() #rule 2: ignore case
-        
-        #rule 3: discard certain letters after the first
-        name = name[0] + name[1:].translate(None,'AEIHOUWY') 
-        
-        #rule 4: consider certain sets of letters equivalent
-        activeSet = 0
-        reducedName = ''
-        for character in name:
-            #rule 5: consecutive members of set are single occurrence
-            if activeSet != 0 and character in equivalents[activeSet]: continue 
-            
-            for setID,charset in equivalents.items():
-                if character in charset:
-                    reducedName += str(setID)
-                    activeSet = setID
-                    break
-            else:
-                reducedName += character
-                activeSet = 0
-        
-        return reducedName  
+        for setID,charset in equivalents.items():
+            if character in charset:
+                reducedName += str(setID)
+                activeSet = setID
+                break
+        else:
+            reducedName += character
+            activeSet = 0
+    
+    return reducedName  
+
+if __name__ == '__main__':
     
     #build a reduced name -> real name dictionary from the supplied list
     inputNameDict = {} 
@@ -59,3 +59,5 @@ if __name__ == '__main__':
     for name in sys.argv[1:]:
         reducedName = reduceName(name)
         if reducedName in inputNameDict: print '\n%s: %s'%(name,inputNameDict[reducedName])
+        
+    
